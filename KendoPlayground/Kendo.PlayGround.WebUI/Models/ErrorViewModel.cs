5353de2a2e1023074106d2cff05df0b535d45342
Ushan Edirisinghe <malindaushan999@gmail.com>
2024-11-02 22:20:50 +0530

    Added sample web project with identity

---
+++ KendoPlayground/Kendo.PlayGround.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Kendo.PlayGround.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
