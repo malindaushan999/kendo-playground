ea985022d2078bb71181e138a33247444ca436e8
Ushan Edirisinghe <malindaushan999@gmail.com>
2024-11-03 08:43:58 +0530

    Added persistence

---
+++ KendoPlayground/Kendo.PlayGround.Persistance/KendoPlayGroundContext.cs
@@ -0,0 +1,27 @@
+﻿
+using Kendo.PlayGround.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Kendo.PlayGround.Persistence;
+
+public partial class KendoPlayGroundContext : DbContext
+{
+    public KendoPlayGroundContext(DbContextOptions<KendoPlayGroundContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customer { get; set; }
+
+    public virtual DbSet<CustomerDetails> CustomerDetails { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new Configurations.CustomerConfiguration());
+        modelBuilder.ApplyConfiguration(new Configurations.CustomerDetailsConfiguration());
+
+        OnModelCreatingGeneratedFunctions(modelBuilder);
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
