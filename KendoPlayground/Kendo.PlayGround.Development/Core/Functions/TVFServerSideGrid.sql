e7d173eb1bc017817b8153215a905ed98ed60db9
Ushan Edirisinghe <malindaushan999@gmail.com>
2024-11-03 08:31:07 +0530

    Added TVF

---
+++ KendoPlayground/Kendo.PlayGround.Development/Core/Functions/TVFServerSideGrid.sql
@@ -0,0 +1,25 @@
+﻿CREATE FUNCTION [dbo].[TVFServerSideGrid]
+(
+	@UserId INT
+)
+RETURNS TABLE
+AS 
+RETURN
+(
+	SELECT 
+		C.CustomerId,
+		C.FirstName,
+		C.LastName,
+		C.Email,
+		C.Phone,
+		CD.[Address],
+		CD.City,
+		CD.[State],
+		CD.ZipCode,
+		CD.Country,
+		CD.DateOfBirth,
+		CD.CreatedDate,
+		CD.ModifiedDate
+	FROM Core.Customer C
+	INNER JOIN Core.CustomerDetails CD ON C.CustomerId = CD.CustomerId
+)
