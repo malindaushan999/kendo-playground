da80781938c87701c54c9b882749ad0ae6990d27
Ushan Edirisinghe <malindaushan999@gmail.com>
2024-11-03 08:31:17 +0530

    Added domain

---
+++ KendoPlayground/Kendo.PlayGround.Domain/Entities/Core/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kendo.PlayGround.Domain.Entities;
+
+[Table("Customer", Schema = "Core")]
+[Index("Email", Name = "UQ_Customer_Email", IsUnique = true)]
+public partial class Customer
+{
+    [Key]
+    public int CustomerID { get; set; }
+
+    [StringLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [StringLength(50)]
+    public string LastName { get; set; } = null!;
+
+    [StringLength(100)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(15)]
+    public string? Phone { get; set; }
+
+    [StringLength(50)]
+    public string CreatedBy { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+
+    [StringLength(50)]
+    public string? ModifiedBy { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    [InverseProperty("Customer")]
+    public virtual ICollection<CustomerDetails> CustomerDetails { get; set; } = new List<CustomerDetails>();
+}
---
+++ KendoPlayground/Kendo.PlayGround.Domain/Entities/Core/CustomerDetails.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kendo.PlayGround.Domain.Entities;
+
+[Table("CustomerDetails", Schema = "Core")]
+public partial class CustomerDetails
+{
+    [Key]
+    public int CustomerDetailsID { get; set; }
+
+    public int CustomerID { get; set; }
+
+    [StringLength(255)]
+    public string Address { get; set; } = null!;
+
+    [StringLength(100)]
+    public string City { get; set; } = null!;
+
+    [StringLength(50)]
+    public string State { get; set; } = null!;
+
+    [StringLength(10)]
+    public string ZipCode { get; set; } = null!;
+
+    [StringLength(50)]
+    public string Country { get; set; } = null!;
+
+    [Column(TypeName = "date")]
+    public DateTime? DateOfBirth { get; set; }
+
+    [StringLength(50)]
+    public string CreatedBy { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+
+    [StringLength(50)]
+    public string? ModifiedBy { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    [ForeignKey("CustomerID")]
+    [InverseProperty("CustomerDetails")]
+    public virtual Customer Customer { get; set; } = null!;
+}
