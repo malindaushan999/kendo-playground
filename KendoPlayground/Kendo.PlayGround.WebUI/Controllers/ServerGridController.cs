0bad61578084df81cc8783bafb40f90b7c87e816
Ushan Edirisinghe <malindaushan999@gmail.com>
2024-11-03 09:26:40 +0530

    Added server controllers

---
+++ KendoPlayground/Kendo.PlayGround.WebUI/Controllers/ServerGridController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kendo.PlayGround.WebUI.Controllers;
+
+[Authorize]
+public class ServerGridController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public
+}
